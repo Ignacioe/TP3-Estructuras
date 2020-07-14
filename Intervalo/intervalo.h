418fd2f686f87ecfb8f6ce66eecabc2f8b65dd4c
Ignacioe <espindola.ignacio@hotmail.com>
2020-07-14 18:42:41 -0300

    agrego intervalos

---
+++ Intervalo/intervalo.h
@@ -0,0 +1,39 @@
+#ifndef __INTERVALO_H__
+#define __INTERVALO_H__
+
+typedef struct {
+  int inicio;
+  int final;
+} Intervalo;
+
+/**
+ * Devuelve un intervalo formado por los extremos dados.
+ */
+Intervalo *intervalo_crear(double inicio, double final);
+
+/**
+ * Destruye el intervalo dado
+ */
+void intervalo_destruir(Intervalo * intervalo);
+
+/**
+ * Indica si el intervalo dado es valido
+ */
+int intervalo_validar(Intervalo * intervalo);
+
+/**
+ * Indica si los dos intervalos dados se intersecan
+ */
+int intervalo_interseca(Intervalo * intervaloA, Intervalo * intervaloB);
+
+/**
+ * Imprime en pantalla el intervalo dado
+ */
+void intervalo_imprimir(Intervalo * intervalo);
+
+/**
+ * Indica si dos intervalos dados son iguales
+ */
+int intervalo_coinciden(Intervalo * intervaloA, Intervalo * intervaloB);
+
+#endif                          /* __INTERVALO_H__ */
